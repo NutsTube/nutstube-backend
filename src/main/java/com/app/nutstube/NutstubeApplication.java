4ddebc7c5c9cf69a59d97a68fd52a02c26464d43
JinyWiny <jiny95.dev@gmail.com>
2025-01-02 21:55:12 +0900

    chore: nutstube 프로젝트 세팅

---
+++ src/main/java/com/app/nutstube/NutstubeApplication.java
@@ -0,0 +1,13 @@
+package com.app.nutstube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutstubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutstubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/nutstube/NutstubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.nutstube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutstubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
